246eafdb0ab044d931c6474a7c077eeddc239af9
BIJAY KUMAR MALLIK <80885955+bluemate777@users.noreply.github.com>
2021-03-27 19:18:19 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,6 +1,12 @@
 import requests
 
+server.listen(port,()=>{  // do not add localhost here if you are deploying it
+    console.log("server listening to port "+port);
+});
+
+'''
 url = 'http://localhost:5000/predict_api'
 r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
 
 print(r.json())
+'''
